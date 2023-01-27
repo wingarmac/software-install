f726a34fba7697883f65a365d51ce6d2b0b87eb5
wingarmac <wingarmac@gmail.com>
2023-01-27 22:09:25 +0100

    Submenu for installlation.txt import/export

---
+++ setup-options/packages.sh
@@ -0,0 +1,53 @@
+#!/bin/sh
+show_menu(){
+    normal=`echo "\033[m"`
+    menu=`echo "\033[36m"` #Blue
+    number=`echo "\033[33m"` #yellow
+    bgred=`echo "\033[41m"`
+    fgred=`echo "\033[31m"`
+    printf "\n${menu}*********************************************${normal}\n"
+    printf "\n${menu}*             ${fgred}Software Menu              ${menu}*${normal}\n"
+    printf "\n${menu}*********************************************${normal}\n"
+    printf "${menu}**${number} 1)${menu} Install software list ${normal}\n"
+    printf "${menu}**${number} 2)${menu} Export software list ${normal}\n"
+    printf "${menu}*********************************************${normal}\n"
+    printf "Please enter a menu option and enter or ${fgred}x to exit. ${normal}"
+    read opt
+}
+
+option_picked(){
+    msgcolor=`echo "\033[01;31m"` # bold red
+    normal=`echo "\033[00;00m"` # normal white
+    message=${@:-"${normal}Error: No message passed"}
+    printf "${msgcolor}${message}${normal}\n"
+}
+
+clear
+show_menu
+while [ $opt != '' ]
+    do
+    if [ $opt = '' ]; then
+      exit;
+    else
+      case $opt in
+        1) clear;
+            option_picked "Option 1 Picked";
+            for pkg in `cat installation.txt`; do sudo apt install -y $pkg; done;
+            show_menu;
+        ;;
+        2) clear;
+            option_picked "Option 2 Picked";
+            exit;
+	    show_menu;
+        ;;
+        x)exit;
+        ;;
+        \n)exit;
+        ;;
+        *)clear;
+            option_picked "Pick an option from the menu";
+            show_menu;
+        ;;
+      esac
+    fi
+done
